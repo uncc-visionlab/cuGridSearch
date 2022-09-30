e0d1e9f5b0f267ed4c5bf31ef2ac7037e8c4f29c
Andrew Willis <arwillis@uncc.edu>
2022-09-30 11:06:12 -0400

    Modifications made to use the cuGridSearch library for new large scale motion compensation autofocusing algorithms.

---
+++ include/cuGridSearch.cuh
@@ -0,0 +1,11 @@
+//
+// Created by arwillis on 9/30/22.
+//
+
+#ifndef UNCC_SAR_FOCUSING_CUGRIDSEARCH_H
+#define UNCC_SAR_FOCUSING_CUGRIDSEARCH_H
+
+#include "cudaTensor.cuh"
+#include "third_party/helper_cuda.h"
+
+#endif //UNCC_SAR_FOCUSING_CUGRIDSEARCH_H
