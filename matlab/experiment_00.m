3ae7321032c05bc6e080320f56ccf45c6c9beccb
Andrew Willis <arwillis@uncc.edu>
2023-01-13 12:33:41 -0500

    Compute results across all 3 datasets.

--- matlab/experiment_00.m
+++ matlab/experiment_00.m
@@ -1,99 +1,133 @@
 clear;
 clc;
 
-% Change these file paths to match your path setup
-dataFolder_root = fullfile('/home','arwillis', 'CLionProjects', 'georeg_exps');
-dataFolder_dataset = fullfile('geo_dataset', 'Concord');
-dataFolder_moving = fullfile(dataFolder_root, dataFolder_dataset, 'moving');
-dataFolder_reference = fullfile(dataFolder_root, dataFolder_dataset, 'fixed');
-dataFolder_fused_output = fullfile(dataFolder_root, 'results', 'Concord');
-gt_data = readtable(fullfile(dataFolder_root, dataFolder_dataset, 'rosgeoregistration_2022_12_21_19_19_13.log'));
+% DATASET_INDEX=1;
+NUM_FILES=2;
+USER_FOLDER='arwillis';
 
-% dataFolder_moving = fullfile('..','src','gpu', 'testImages','reg','sar');
-% dataFolder_reference = fullfile('..','src','gpu', 'testImages','reg','gmap');
-execute_binary='../cmake-build-debug/src/cpu/multispectral_ImageMatcher';
+datasetName{1}='EPIC';
+datafileGTName{1}='rosgeoregistration_2022_12_21_23_38_22.log';
+datasetName{2}='Concord';
+datafileGTName{2}='rosgeoregistration_2022_12_21_19_19_13.log';
+datasetName{3}='Isleta';
+datafileGTName{3}='rosgeoregistration_2022_12_21_21_28_42.log';
 
-imds_reference = imageDatastore(dataFolder_reference, 'IncludeSubfolders',true,'LabelSource','foldernames');
-imds_moving = imageDatastore(dataFolder_moving, 'IncludeSubfolders',true,'LabelSource','foldernames');
-
-arg_reference_image='--i_ref';
-arg_moving_image='--i_mov';
-arg_fused_output_image='-f';
-image_ref='';
-image_mov='';
-
-H_estimated_list = [];
-H_ground_truth_list = [];
-scale_list = [];
-runtime_list = [];
-
-for fileIdx=1:length(imds_moving.Files)
-    time_start = tic;
-    image_ref=imds_reference.Files(fileIdx);
-    image_mov=imds_moving.Files(fileIdx);
-    indices=find('/' == image_ref{1});
-    image_out_fused = strcat(dataFolder_fused_output,'/',image_ref{1}(indices(end)+1:end-9),'fused.png');
-    command=strcat(execute_binary," ", ...
-        arg_reference_image," ",image_ref," ", ...
-        arg_moving_image," ",image_mov, " ", ...
-        arg_fused_output_image," ",image_out_fused)
+for DATASET_INDEX=1:3
+    % Change these file paths to match your path setup
+    % dataFolder_root = fullfile('/home','arwillis', 'CLionProjects', 'georeg_exps');
+    dataFolder_root = fullfile('/home','server', 'SAR');
+    dataFolder_results = fullfile('/home',USER_FOLDER, 'CLionProjects', 'georeg_exps');
     
-    [status,cmdout] = system(command);
-    % add output of homography to cuGridSearch
-    runtime = toc(time_start);
-    result = regexp(cmdout,"Answer\[([^\]]*)]\s+Scale_factor\[([^\]]*)]", "tokens");
-    H_estimated = reshape(str2num(result{1}{1}),1,[]);
-    H_estimated = [H_estimated 1]; % Only has 8 values originally, needs h33 to be 1
-    scale_factor = reshape(str2num(result{1}{2}),1,[]);
-    H_estimated_list = [H_estimated_list; H_estimated];
-    H_estimated = reshape(H_estimated, 3, 3)';
-    scale_list = [scale_list; scale_factor];    
-    runtime_list = [runtime_list; runtime];
+    dataFolder_dataset = fullfile('geo_dataset', datasetName{DATASET_INDEX});
+    dataFolder_moving = fullfile(dataFolder_root, dataFolder_dataset, 'moving');
+    dataFolder_reference = fullfile(dataFolder_root, dataFolder_dataset, 'fixed');
+    dataFolder_fused_output = fullfile(dataFolder_results, datasetName{DATASET_INDEX});
+    dataFolder_compared_fused_output = fullfile(dataFolder_results, datasetName{DATASET_INDEX},'comparisons');
     
-    I_fixed = imread(image_ref{1});
-    I_moving = imread(image_mov{1});
-    I_fixed_scaled = imread('image_fixed_resized.png');
+    gt_data = readtable(fullfile(dataFolder_root, dataFolder_dataset, datafileGTName{DATASET_INDEX}));
     
-    Hvals = table2array(gt_data(fileIdx,18:26));
-    H_ground_truth = reshape(Hvals, 3,3)';
-
-    scale = scale_list(fileIdx, :);
-    S = [scale(1) 0 0; 0 scale(2) 0; 0 0 1];
-
-    % Formula: B' = (S * H * S^-1) * S * A
-    H_ground_truth_rescaled = inv(S) * H_ground_truth * S 
-    H_ground_truth_rowvec = reshape(H_ground_truth_rescaled', 1,[]);
-    H_ground_truth_list = [H_ground_truth_list; H_ground_truth_rowvec];
+    % dataFolder_moving = fullfile('..','src','gpu', 'testImages','reg','sar');
+    % dataFolder_reference = fullfile('..','src','gpu', 'testImages','reg','gmap');
+    execute_binary='../cmake-build-debug/src/cpu/multispectral_ImageMatcher';
     
-    [rows, cols, ~] = size(I_fixed_scaled);
-
-    tform = maketform('projective', [scale(1) 0 0; 0 scale(2) 0; 0 0 1]');
-    I_fixed_scaled2 = imtransform(I_fixed, tform, 'XData',[1 cols],'YData',[1 rows], 'XYScale',1);
-
-    tform = maketform('projective', H_estimated');
-    I_moving_scaled_estimated = imtransform(I_moving, tform, 'XData',[1 cols],'YData',[1 rows], 'XYScale',1);
+    imds_reference = imageDatastore(dataFolder_reference, 'IncludeSubfolders',true,'LabelSource','foldernames');
+    imds_moving = imageDatastore(dataFolder_moving, 'IncludeSubfolders',true,'LabelSource','foldernames');
     
-    tform = maketform('projective', H_ground_truth_rescaled');
-    I_moving_ground_truth_scaled = imtransform(I_moving, tform, 'XData',[1 cols],'YData',[1 rows], 'XYScale',1);
-
-    [rows, cols, ~] = size(I_fixed);
-    tform = maketform('projective', H_ground_truth');
-    I_moving_ground_truth = imtransform(I_moving, tform, 'XData',[1 cols],'YData',[1 rows], 'XYScale',1);
+    arg_reference_image='--i_ref';
+    arg_moving_image='--i_mov';
+    arg_fused_output_image='-f';
+    image_ref='';
+    image_mov='';
     
-    I_fused = zeros(rows, cols, 3, 'uint8');
-    I_fused(:,:,2) = uint8(I_fixed(:,:,1));
-    I_fused(:,:,3) = uint8(2*I_moving_ground_truth(:,:,1));
-    subplot(1,2,1), imshow(I_fused);
+    H_estimated_list = [];
+    H_ground_truth_list = [];
+    scale_list = [];
+    runtime_list = [];
     
-    [rows, cols, ~] = size(I_fixed_scaled);
-    I_fused_scaled = zeros(rows, cols, 3, 'uint8');
-    I_fused_scaled(:,:,2) = uint8(I_fixed_scaled(:,:,1));
-    I_fused_scaled(:,:,3) = uint8(2*I_moving_scaled_estimated(:,:,1));
-    subplot(1,2,2), imshow(I_fused_scaled);
+    f1_handle = figure(1);
+    skip = round(length(imds_moving.Files)/(NUM_FILES+1))
     
-    aaa=1;
+    for fileIdx=1:skip:length(imds_moving.Files)
+        time_start = tic;
+        image_ref=imds_reference.Files(fileIdx);
+        image_mov=imds_moving.Files(fileIdx);
+        indices=find('/' == image_ref{1});
+        image_out_fused = strcat(dataFolder_fused_output,'/',image_ref{1}(indices(end)+1:end-9),'fused.png');
+        %command=strcat(execute_binary," ", ...
+        %    arg_reference_image," ",image_ref," ", ...
+        %    arg_moving_image," ",image_mov, " ", ...
+        %    arg_fused_output_image," ",image_out_fused)
+        command=strcat(execute_binary," ", ...
+            arg_reference_image," ",image_ref," ", ...
+            arg_moving_image," ",image_mov)
+        
+        [status,cmdout] = system(command);
+        % add output of homography to cuGridSearch
+        runtime = toc(time_start);
+        result = regexp(cmdout,"Answer\[([^\]]*)]\s+Scale_factor\[([^\]]*)]", "tokens");
+        H_estimated = reshape(str2num(result{1}{1}),1,[]);
+        H_estimated = [H_estimated 1]; % Only has 8 values originally, needs h33 to be 1
+        scale_factor = reshape(str2num(result{1}{2}),1,[]);
+        H_estimated_list = [H_estimated_list; H_estimated];
+        H_estimated = reshape(H_estimated, 3, 3)';
+        scale_list = [scale_list; scale_factor];
+        runtime_list = [runtime_list; runtime];
+        
+        I_fixed = imread(image_ref{1});
+        I_moving = imread(image_mov{1});
+        I_fixed_scaled = imread('image_fixed_resized.png');
+        
+        Hvals = table2array(gt_data(fileIdx,18:26));
+        H_ground_truth = reshape(Hvals, 3,3)';
+        
+        scale = scale_list(fileIdx, :);
+        S = [scale(1) 0 0; 0 scale(2) 0; 0 0 1];
+        
+        % Formula: B' = (S * H * S^-1) * S * A
+        H_ground_truth_rescaled = inv(S) * H_ground_truth * S
+        H_ground_truth_rowvec = reshape(H_ground_truth_rescaled', 1,[]);
+        H_ground_truth_list = [H_ground_truth_list; H_ground_truth_rowvec];
+        
+        [rows, cols, ~] = size(I_fixed_scaled);
+        
+        tform = maketform('projective', [scale(1) 0 0; 0 scale(2) 0; 0 0 1]');
+        I_fixed_scaled2 = imtransform(I_fixed, tform, 'XData',[1 cols],'YData',[1 rows], 'XYScale',1);
+        
+        tform = maketform('projective', H_estimated');
+        I_moving_scaled_estimated = imtransform(I_moving, tform, 'XData',[1 cols],'YData',[1 rows], 'XYScale',1);
+        
+        tform = maketform('projective', H_ground_truth_rescaled');
+        I_moving_ground_truth_scaled = imtransform(I_moving, tform, 'XData',[1 cols],'YData',[1 rows], 'XYScale',1);
+        
+        [rows, cols, ~] = size(I_fixed);
+        tform = maketform('projective', H_ground_truth');
+        I_moving_ground_truth = imtransform(I_moving, tform, 'XData',[1 cols],'YData',[1 rows], 'XYScale',1);
+        
+        I_fused = zeros(rows, cols, 3, 'uint8');
+        I_fused(:,:,2) = uint8(I_fixed(:,:,1));
+        I_fused(:,:,3) = uint8(2*I_moving_ground_truth(:,:,1));
+        
+        [rows, cols, ~] = size(I_fixed_scaled);
+        I_fused_scaled = zeros(rows, cols, 3, 'uint8');
+        I_fused_scaled(:,:,2) = uint8(I_fixed_scaled(:,:,1));
+        I_fused_scaled(:,:,3) = uint8(2*I_moving_scaled_estimated(:,:,1));
+        
+        
+        basename = image_ref{1}(indices(end)+1:end-9);
+        image_out_fused_ground_truth = fullfile(dataFolder_fused_output,strcat(basename,'fused_gt.png'));
+        image_out_fused_estimated = fullfile(dataFolder_fused_output,strcat(basename,'fused_est.png'));
+        imwrite(I_fused, image_out_fused_ground_truth);
+        imwrite(I_fused_scaled, image_out_fused_estimated);
+        
+        %f1_handle = figure(1);
+        set(0,'CurrentFigure',f1_handle);
+        subplot(1,2,1), imshow(I_fused), title('Ground Truth');
+        subplot(1,2,2), imshow(I_fused_scaled), title('Estimated');
+        image_out_comp_fused = fullfile(dataFolder_compared_fused_output,strcat(basename,'fused_comp.png'));
+        saveas(f1_handle, image_out_comp_fused)
+        aaa=1;
+    end
 end
-
 
 
 H_ground_truth_list = [];
